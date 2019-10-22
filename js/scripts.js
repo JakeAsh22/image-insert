d1658d7223a653e9cadd2d014e3af238d579f91d
Jake Ash and Jordan Safford <jake@>
2019-10-22 16:44:36 -0700

    Will not make code fully work, but will play with deleting the image

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,18 @@
 $(document).ready(function()
 {
-  $(".clickable1").click(function()
+  $("#clickable1").click(function()
   {
     $("p#python").before('<img src="img/ball-python.jpg" >');
-    $("p#python").children("img src").first().click(function()
+    $('#clickable1').click(function() {
+  $(this).remove();
+});
+  });
+  $("#clickable2").click(function()
+  {
+    $("p#boa").after('<img src="img/rubber-boa.jpg">');
+    $("div").children("p#boa").first().click(function()
     {
       $(this).remove();
     });
-  });
-  $(".clickable2").click(function()
-  {
-    $("p#boa").after('<img src="img/rubber-boa.jpg">');
   });
 });
